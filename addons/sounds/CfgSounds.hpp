51003a5f4e2227618cb6e6042e8f1ebbd83c451a
Nomas <78245023+Nomas-X@users.noreply.github.com>
2026-04-29 19:42:59 +0300

    Added sounds for mapleSyrup
    - Added `mapleSyrup_CAPOnStation.ogg`
    - Added `mapleSyrup_ETA30Seconds.ogg`
    - Added `mapleSyrup_LZIsHot.ogg`

--- addons/sounds/CfgSounds.hpp
+++ addons/sounds/CfgSounds.hpp
@@ -37,4 +37,58 @@ class CfgSounds
 		};
 		duration=182;
 	};
+	class GVAR(mapleSyrup_CAPOnStation)
+	{
+		author="AET";
+		name="AET (Maple Syrup) CAP on Station";
+		sound[]=
+		{
+			QPATHTOF(data\nomas\mapleSyrup_CAPOnStation.ogg),
+			5,
+			1,
+			20
+		};
+		titles[]=
+		{
+			0,
+			""
+		};
+		duration=2;
+	};
+	class GVAR(mapleSyrup_ETA30Seconds)
+	{
+		author="AET";
+		name="AET (Maple Syrup) ETA 30 Seconds";
+		sound[]=
+		{
+			QPATHTOF(data\nomas\mapleSyrup_ETA30Seconds.ogg),
+			5,
+			1,
+			50
+		};
+		titles[]=
+		{
+			0,
+			""
+		};
+		duration=1;
+	};
+	class GVAR(mapleSyrup_LZIsHot)
+	{
+		author="AET";
+		name="AET (Maple Syrup) LZ is Hot";
+		sound[]=
+		{
+			QPATHTOF(data\nomas\mapleSyrup_LZIsHot.ogg),
+			5,
+			1,
+			50
+		};
+		titles[]=
+		{
+			0,
+			""
+		};
+		duration=3;
+	};
 };
